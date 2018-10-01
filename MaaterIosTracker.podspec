46af0e80a8bb675b9637c438936014574610887c
Mohammed O. Tillawy <tillawy@gmail.com>
2018-10-01 12:58:54 +0300

    initial import

--- MaaterIosTracker.podspec
+++ MaaterIosTracker.podspec
@@ -10,6 +10,9 @@ Pod::Spec.new do |s|
   s.name             = 'MaaterIosTracker'
   s.version          = '0.1.0'
   s.summary          = 'A short description of MaaterIosTracker.'
+  s.swift_version    = '4.2'
+
+
 
 # This description is used to generate tags and improve search results.
 #   * Think: What does it do? Why did you write it? What is the focus?
@@ -20,12 +23,15 @@ Pod::Spec.new do |s|
   s.description      = <<-DESC
 TODO: Add long description of the pod here.
                        DESC
+s.homepage         = 'https://github.com/arabiaweather/maater-ios-tracker-pod'
+# s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
+s.license          = { :type => 'MIT', :file => 'LICENSE' }
+s.author           = { 'Mohammed O. Tillawy' => 'mohammed.tillawy@arabiaweather.com' }
+s.source           = {
+:git => 'https://github.com/arabiaweather/maater-ios-tracker-pod.git' ,
+:tag => s.version.to_s
+}
 
-  s.homepage         = 'https://github.com/tillawy/MaaterIosTracker'
-  # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
-  s.license          = { :type => 'MIT', :file => 'LICENSE' }
-  s.author           = { 'tillawy' => 'tillawy@gmail.com' }
-  s.source           = { :git => 'https://github.com/tillawy/MaaterIosTracker.git', :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
   s.ios.deployment_target = '8.0'
@@ -39,4 +45,7 @@ TODO: Add long description of the pod here.
   # s.public_header_files = 'Pod/Classes/**/*.h'
   # s.frameworks = 'UIKit', 'MapKit'
   # s.dependency 'AFNetworking', '~> 2.3'
+
+  s.dependency  'Socket.IO-Client-Swift', '~> 13.3.0'
+
 end
---
+++ MaaterIosTracker/Classes/MaaterIosTracker.swift
@@ -0,0 +1,315 @@
+
+//
+//  AppDelegate+tracking.swift
+//  TrackerIosPOC
+//
+//  Created by Mohammed O. Tillawy on 9/28/18.
+//  Copyright © 2018 MOH TILLAWY. All rights reserved.
+//
+
+import Foundation
+import SocketIO
+
+
+
+public class Tracker {
+
+    public static let shared = Tracker()
+    
+    public init() {}
+
+    struct Holder {
+        static var _manager:SocketManager = SocketManager(socketURL: URL(string: "http://localhost:8282")!, config: [.log(true), .compress])
+        static var _socket: SocketIOClient = Holder._manager.defaultSocket
+        static var _userId: Int = 0
+        static var _clientId: Int = 0
+        static var _name: String = ""
+        static var _email: String = ""
+        static var _trackingId: Int = 0
+    }
+    
+    var manager:SocketManager {
+        get {
+            return Holder._manager
+        }
+        set(newValue) {
+            Holder._manager = newValue
+        }
+    }
+    
+    var socket:SocketIOClient {
+        get {
+            return Holder._socket
+        }
+        set(newValue) {
+            Holder._socket = newValue
+        }
+    }
+    
+    
+    var userId:Int {
+        get {
+            return Holder._userId
+        }
+        set(newValue) {
+            Holder._userId = newValue
+        }
+    }
+    
+    
+    var clientId:Int {
+        get {
+            return Holder._clientId
+        }
+        set(newValue) {
+            Holder._clientId = newValue
+        }
+    }
+    
+    
+    var name: String {
+        get {
+            return Holder._name
+        }
+        set(newValue) {
+            Holder._name = newValue
+        }
+    }
+    
+    
+    var email: String {
+        get {
+            return Holder._email
+        }
+        set(newValue) {
+            Holder._email = newValue
+        }
+    }
+    
+    var trackingId:Int {
+        get {
+            return Holder._trackingId
+        }
+        set(newValue) {
+            Holder._trackingId = newValue
+        }
+    }
+    
+    
+    public func trackUser(userId: Int, clientId: Int, fullname: String, email: String ){
+        
+        
+        guard self.socket.status == .connected else {
+            print("NOT CONNECTED TO TRACK")
+            return
+        }
+        
+        if (trackingId != 0){
+            print("allready tracked, trackingId \(trackingId)")
+            return
+        }
+        
+        let c = TrackCommand(userId: userId, clientId: clientId, name: fullname , email: email)
+        let encoder = JSONEncoder()
+        let d = try! encoder.encode(c)
+        let json = String(data: d, encoding: .utf8)!
+        let K_TRACK = "track"
+        self.socket.emit(K_TRACK, json)
+        
+        self.userId = userId
+        self.clientId = clientId
+        self.name = fullname
+        self.email = email
+    }
+    
+    public func connectTracking(env: TrackingEnvironment){
+        
+        if (socket.status == .connected || socket.status == .connecting){
+            return
+        }
+        
+        var server =  "http://localhost:8282"
+        let isSecure = env != .development
+        let isVerboseLogging = env == .development
+        
+        switch env {
+        case .development:
+            server = "http://macbook-air.duckdns.org:8282"
+        case .staging:
+            server = "https://staging.tracker.devops.arabiaweather.com"
+        case .production:
+            server = "https://production.tracker.devops.arabiaweather.com"
+        }
+        
+        manager = SocketManager(socketURL: URL(string: server)!, config: [.log(isVerboseLogging), .secure(isSecure), .compress])
+        
+        socket = manager.defaultSocket
+        
+        socket.on(clientEvent: .connect) {data, ack in
+            print("socket connected")
+            if (self.userId != 0 && self.clientId != 0 && !self.name.isEmpty && !self.email.isEmpty ){
+                self.trackUser(userId: self.userId, clientId: self.clientId, fullname: self.name, email: self.email)
+            }
+        }
+        
+        socket.on("tracked", callback: { data , ack  in
+            if let arr = data as? Array<[String:Any]>, let dic = arr.first , let pvid = dic["id"] as? Int{
+                print("tracked :\n\n", pvid)
+                self.trackingId = pvid
+            }
+        })
+        
+        socket.connect()
+    }
+    
+    public func disconnectTracking(){
+        socket.disconnect()
+        self.trackingId = 0
+    }
+}
+
+
+public enum TrackingEnvironment {
+    case development
+    case staging
+    case production
+}
+
+
+struct TrackCommand : Codable {
+    
+    enum CodingKeys: String, CodingKey
+    {
+        case command
+        case userId = "user_id"
+        case email
+        case name
+        case clientId = "client_id"
+        case os = "os"
+        case osVersion = "os_version"
+        case clientType = "client_type"
+        case deviceModel = "device_model"
+        case deviceType = "device_type"
+    }
+    
+    let command: String = "track"
+    let userId: Int
+    let name: String
+    let email: String
+    let clientId: Int
+    let os: String = UIDevice.current.systemName
+    let osVersion: String = UIDevice.current.systemVersion
+    let deviceModel: String = UIDevice.modelName
+    let deviceType = 1
+    let clientType = "mobile"
+    
+    
+    init( userId: Int, clientId: Int, name: String, email: String ) {
+        self.userId = userId
+        self.clientId = clientId
+        self.name = name
+        self.email = email
+    }
+    
+}
+
+
+
+public extension UIDevice {
+    
+    static let modelName: String = {
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let machineMirror = Mirror(reflecting: systemInfo.machine)
+        let identifier = machineMirror.children.reduce("") { identifier, element in
+            guard let value = element.value as? Int8, value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
+        }
+        
+        func mapToDevice(identifier: String) -> String { // swiftlint:disable:this cyclomatic_complexity
+            #if os(iOS)
+            switch identifier {
+            case "iPod5,1":                                 return "iPod Touch 5"
+            case "iPod7,1":                                 return "iPod Touch 6"
+            case "iPhone3,1", "iPhone3,2", "iPhone3,3":     return "iPhone 4"
+            case "iPhone4,1":                               return "iPhone 4s"
+            case "iPhone5,1", "iPhone5,2":                  return "iPhone 5"
+            case "iPhone5,3", "iPhone5,4":                  return "iPhone 5c"
+            case "iPhone6,1", "iPhone6,2":                  return "iPhone 5s"
+            case "iPhone7,2":                               return "iPhone 6"
+            case "iPhone7,1":                               return "iPhone 6 Plus"
+            case "iPhone8,1":                               return "iPhone 6s"
+            case "iPhone8,2":                               return "iPhone 6s Plus"
+            case "iPhone9,1", "iPhone9,3":                  return "iPhone 7"
+            case "iPhone9,2", "iPhone9,4":                  return "iPhone 7 Plus"
+            case "iPhone8,4":                               return "iPhone SE"
+            case "iPhone10,1", "iPhone10,4":                return "iPhone 8"
+            case "iPhone10,2", "iPhone10,5":                return "iPhone 8 Plus"
+            case "iPhone10,3", "iPhone10,6":                return "iPhone X"
+            case "iPhone11,2":                              return "iPhone XS"
+            case "iPhone11,4", "iPhone11,6":                return "iPhone XS Max"
+            case "iPhone11,8":                              return "iPhone XR"
+            case "iPad2,1", "iPad2,2", "iPad2,3", "iPad2,4":return "iPad 2"
+            case "iPad3,1", "iPad3,2", "iPad3,3":           return "iPad 3"
+            case "iPad3,4", "iPad3,5", "iPad3,6":           return "iPad 4"
+            case "iPad4,1", "iPad4,2", "iPad4,3":           return "iPad Air"
+            case "iPad5,3", "iPad5,4":                      return "iPad Air 2"
+            case "iPad6,11", "iPad6,12":                    return "iPad 5"
+            case "iPad7,5", "iPad7,6":                      return "iPad 6"
+            case "iPad2,5", "iPad2,6", "iPad2,7":           return "iPad Mini"
+            case "iPad4,4", "iPad4,5", "iPad4,6":           return "iPad Mini 2"
+            case "iPad4,7", "iPad4,8", "iPad4,9":           return "iPad Mini 3"
+            case "iPad5,1", "iPad5,2":                      return "iPad Mini 4"
+            case "iPad6,3", "iPad6,4":                      return "iPad Pro 9.7 Inch"
+            case "iPad6,7", "iPad6,8":                      return "iPad Pro 12.9 Inch"
+            case "iPad7,1", "iPad7,2":                      return "iPad Pro 12.9 Inch 2. Generation"
+            case "iPad7,3", "iPad7,4":                      return "iPad Pro 10.5 Inch"
+            case "AppleTV5,3":                              return "Apple TV"
+            case "AppleTV6,2":                              return "Apple TV 4K"
+            case "AudioAccessory1,1":                       return "HomePod"
+            case "i386", "x86_64":                          return "Simulator \(mapToDevice(identifier: ProcessInfo().environment["SIMULATOR_MODEL_IDENTIFIER"] ?? "iOS"))"
+            default:                                        return identifier
+            }
+            #elseif os(tvOS)
+            switch identifier {
+            case "AppleTV5,3": return "Apple TV 4"
+            case "AppleTV6,2": return "Apple TV 4K"
+            case "i386", "x86_64": return "Simulator \(mapToDevice(identifier: ProcessInfo().environment["SIMULATOR_MODEL_IDENTIFIER"] ?? "tvOS"))"
+            default: return identifier
+            }
+            #endif
+        }
+        
+        return mapToDevice(identifier: identifier)
+    }()
+    
+}
+
+
+
+
+
+enum SchemeEnum: String, Codable
+{
+    case Http = "http"
+    case Https = "https"
+}
+
+
+struct Connection: Codable {
+    
+    let host: String
+    let scheme: SchemeEnum
+    let isSecure: Bool
+    
+    init(host: String, scheme: String ){
+        self.host = host
+        var p1 = SchemeEnum.Https
+        if let p2 = SchemeEnum(rawValue: scheme){
+            p1 = p2
+        }
+        self.scheme = p1
+        self.isSecure = self.scheme == .Https
+    }
+    
+}
